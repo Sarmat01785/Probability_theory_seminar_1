9c0a84c244428fd3d30c40a07c64c9437d9e7686
Sarmat <Spirator@Yandex.ru>
2024-03-06 21:04:17 +0700

    The end

--- Probability_Theory_Task_3.py
+++ Probability_Theory_Task_3.py
@@ -3,3 +3,27 @@
 с цифрами от 0 до 9. Код содержит три цифры, которые нужно нажать одновременно.
 Какова вероятность того, что человек, не знающий код, откроет дверь с первой попытки?
 """
+import math
+
+
+# Функция для вычисления биномиального коэффициента (сочетаний)
+def binomial_coefficient(n, k):
+    return math.factorial(n) // (math.factorial(k) * math.factorial(n - k))
+
+
+# Количество кнопок на замке
+buttons = 10
+
+# Количество цифр в коде
+code_length = 3
+
+# Рассчитываем количество возможных уникальных комбинаций
+combinations = binomial_coefficient(buttons, code_length)
+
+# Вероятность угадать код с первой попытки
+probability = 1 / combinations
+
+# Округляем вероятность до 5 знаков после запятой
+# probability = round(probability, 5)
+
+print(f"Вероятность открыть замок с первой попытки: {probability}")
